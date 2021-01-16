a7b227a15d6e846a0376601668d0db992efe316a
Evann Gehin <evann.gehin@ezcorp.io>
2021-01-16 02:23:51 +0100

    Suppression commentaires inutiles

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,6 @@ import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 
-
 void main() => runApp(
   MaterialApp(
     title: "MeteoApp",
